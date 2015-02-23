0b20b5e33131e66f4c0531ecf1849ef1ed248e5d
Uzi Eilon <PerfectoMobile256@gmail.com>
2015-02-23 11:43:38 -0500

    Update testExe.java

--- src/test/java/testExe.java
+++ src/test/java/testExe.java
@@ -39,7 +39,7 @@ public class testExe {
 	public Object[][] testSumInput() {
 		return new Object[][] { 
 				// build dynamic list 
-			//	{ "Device", "0149BCA71700D01F" }, 
+		 		{ "Device", "0149BCA71700D01F" }, 
 		 	 	{ "Device", "39F3DA5531ADBE2A05CFF4D65E43A2C38D3D595A" },
 		 	 	{ "Device", "5CDCB21A"},
 		 	 	{ "Device", "57AF4ED9"},
