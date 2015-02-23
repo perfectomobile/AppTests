33cfaa0301e7f941ab043330b2e97024472168b9
Uzi Eilon <PerfectoMobile256@gmail.com>
2015-02-23 10:50:10 -0500

    Update logIn.java

--- src/test/java/logIn.java
+++ src/test/java/logIn.java
@@ -92,14 +92,6 @@ public class logIn {
 			//download Report 
 			_device.close();
 			PMdriver.quit();	
-			InputStream rep = PMdriver.downloadReport(MediaType.PDF);
-			File repFile = new File("C:\\test\\Starbucks"+_device+".pdf");
-			try {
-				FileUtils.write(rep, repFile);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 		}
 	}
 	
